c8f72d360095bb38e9057d01beea1d2f8f637727
João Calado <joao.calado@inm.pt>
2025-04-21 18:57:37 +0100

    fixed stoping condition

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def main():
 
     try:
         logging.info("Running for: " + str(len(sys.argv) - 1) + " cryptocurrencies")
-        if(len(sys.argv) < 1):
+        if(len(sys.argv) - 1 < 1):
             logging.info("Missing at least one argument, exiting...")
             exit()
 
